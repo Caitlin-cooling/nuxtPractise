a5db0522f56d460739679cf623efbd05a1062054
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-02-18 15:36:48 +0000

    Adds product pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <nuxt-link to="/users">Click for users</nuxt-link>
+    <nuxt-link to="/products">Click for products</nuxt-link>
   </div>
 </template>
 
---
+++ pages/products/_id/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is the product page of product with ID: {{ $route.params.id }}</h1>
+  </div>
+</template>
