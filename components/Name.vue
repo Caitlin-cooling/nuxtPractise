89968983704e5b2519a78e30d41687709c9c6f37
Caitlin-cooling <ccooling@deloitte.co.uk>
2020-02-28 08:39:58 +0000

    Playing around with conditional rendering

---
+++ components/Name.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>The name field is active: {{ active }}</p>
+    <label for="name">Name</label>
+    <input 
+      type="text"
+      id="name"
+      name="name"
+      @focus="active=true"
+      @blur="active=false"
+    >
+    <!-- what about when the button is active? -->
+    <button v-if="active">Confirm</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: false
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
   <div class="container">
     <nuxt-link to="/users">Click for users</nuxt-link><br/>
     <nuxt-link to="/products">Click for products</nuxt-link>
+    <Name />
   </div>
 </template>
+
+<script>
+import Name from '~/components/Name'
+export default {
+  components: { Name }
+}
+</script>
 
